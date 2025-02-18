2cb1952209e727e1a95d099bc6b3a247972018e9
JaumeGandara <jaumegandara@gmail.com>
2025-02-18 16:57:54 +0100

    feat: :sparkles: -add route in index

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,24 @@ import express, { type Express } from 'express';
 import { createConnection } from './config/db.js';
 import apiRouter from './api/index.js';
 import cors from 'cors';
+import authRouter from './api/routers/auth.js';
 
 const app: Express = express();
 const port: string = process.env.HOST_PORT ?? '3000';
 
-// Configuración de CORS
 app.use(cors({
   origin: 'http://localhost:3001',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// Conectar a la base de datos Supabase/PostgreSQL
 console.log('🌐INFO: Starting database connection...');
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const db = await createConnection();
-app.set('db', db); // Guardar la conexión en la app para usar en otros módulos
+app.set('db', db);
 
 app.use(express.urlencoded({ extended: true }));
 
-// Rutas de la API
 app.use('/api/v1', apiRouter);
+app.use('/auth', authRouter);
 
 export default app;
