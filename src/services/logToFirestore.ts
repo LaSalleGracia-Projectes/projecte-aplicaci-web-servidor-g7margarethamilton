b2f098ea5b16be4a0ef869a32cf29710d2f62994
JaumeGandara <jaumegandara@gmail.com>
2025-02-20 18:55:54 +0100

    fix: :bug: -manual delete

--- src/services/logToFirestore.ts
+++ src/services/logToFirestore.ts
@@ -1,33 +1,4 @@
 import { db } from "../config/firebaseConfig.js";
-import { firebase_error, firebase_log } from "./../logger.js";
-
-const deleteOldLogs = async () => {
-    try {
-        const today = new Date().toISOString().split("T")[0];
-        const logsRef = db.collection("logs");
-        const snapshot = await logsRef.orderBy("timestamp").get();
-
-        if (snapshot.empty) return;
-
-        const batch = db.batch();
-        let deletedCount = 0;
-
-        snapshot.forEach((doc) => {
-            const logDate = doc.data().timestamp.split("T")[0];
-            if (logDate !== today) {
-                batch.delete(doc.ref);
-                deletedCount++;
-            }
-        });
-
-        if (deletedCount > 0) {
-            await batch.commit();
-            firebase_log(`🗑 INFO: Deleted ${deletedCount} old logs.`);
-        }
-    } catch (error) {
-        firebase_error(`❌ ERROR deleting old logs: ${error}`);
-    }
-};
 
 const logToFirestore = async (level: string, message: string) => {
     try {
@@ -37,13 +8,8 @@ const logToFirestore = async (level: string, message: string) => {
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
-        firebase_error(`❌ ERROR saving log: ${error}`);
+        console.error(`❌ ERROR saving log: ${error}`);
     }
 };
-
-
-setInterval(() => {
-    deleteOldLogs();
-}, 24 * 60 * 60 * 1000);
 
 export default logToFirestore;
