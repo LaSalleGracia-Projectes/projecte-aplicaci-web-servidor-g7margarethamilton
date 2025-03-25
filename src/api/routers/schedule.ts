ba5c9a54face53d24f4dc7509f4b38a7e67324c5
JaumeGandara <jaumegandara@gmail.com>
2025-03-25 15:46:11 +0100

    feat: :sparkles: -schedule get by id

--- src/api/routers/schedule.ts
+++ src/api/routers/schedule.ts
@@ -23,4 +23,29 @@ router.get('/', async (req: Request, res: Response) => {
     }
 });
 
+/**
+ * GET: Una agenda per ID (només si és de l'usuari o si és admin)
+ * URL: /api/v1/schedule/:id
+ */
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    try {
+        const user = await sql`SELECT is_admin FROM users WHERE email = ${userId}`;
+        const isAdmin = user[0]?.is_admin;
+
+        const schedule = await sql`SELECT * FROM schedule WHERE id = ${id}`;
+        if (schedule.length === 0) return res.status(404).json({ message: 'Agenda no trobada' });
+
+        if (schedule[0].email !== userId && !isAdmin) {
+            return res.status(403).json({ message: 'No tens permís per veure aquesta agenda' });
+        }
+
+        res.json(schedule[0]);
+    } catch (error: any) {
+        res.status(500).json({ message: 'Error al obtenir l’agenda' });
+    }
+});
+
 export default router;
